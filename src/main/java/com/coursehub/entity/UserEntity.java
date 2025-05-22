0a0c4cb18eceafd60b78974c31763af30b469403
NhatNhu <nhudinhnhat2004@gmail.com>
2025-05-22 23:34:26 +0700

    ok

--- src/main/java/com/coursehub/entity/UserEntity.java
+++ src/main/java/com/coursehub/entity/UserEntity.java
@@ -2,12 +2,16 @@ package com.coursehub.entity;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class UserEntity extends BaseEntity{
 
     @Column(unique = true, nullable = false)
