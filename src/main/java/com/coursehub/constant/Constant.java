eb5e64aa044e9b9f84d1be126f5c8f76a6968b4d
kl3inIT <dathip04@gmail.com>
2025-06-08 00:06:14 +0700

    change domain

--- src/main/java/com/coursehub/constant/Constant.java
+++ src/main/java/com/coursehub/constant/Constant.java
@@ -2,7 +2,7 @@ package com.coursehub.constant;
 
 public class Constant {
     public static class CommonConstants {
-        public static final String SUCCESS = "com.coursehub.constant.CommonConstans.SUCCESSS";
+        public static final String SUCCESS = "com.coursehub.constant.CommonConstans.SUCCESS";
 
     }
 }
