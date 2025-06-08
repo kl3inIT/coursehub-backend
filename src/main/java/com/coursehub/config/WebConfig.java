d093a7298f6943678b47e4642ed01822f2603b6c
NhatNhu <nhudinhnhat2004@gmail.com>
2025-06-08 23:01:54 +0700

    payment ok

--- src/main/java/com/coursehub/config/WebConfig.java
+++ src/main/java/com/coursehub/config/WebConfig.java
@@ -6,11 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Áp dụng cho tất cả endpoint
-                .allowedOrigins("http://localhost:3000", "https://coursehub.space") // Domain frontend
+                .allowedOrigins("http://localhost:3000", "https://coursehub.space", "https://coursehub.io.vn") // Domain frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Các phương thức HTTP cho phép
                 .allowedHeaders("*") // Cho phép tất cả header
                 .allowCredentials(true) // Cho phép gửi cookie hoặc thông tin xác thực
