ee634535b7ae08ed44655408600654348ba553d2
kl3inIT <dathip04@gmail.com>
2025-07-24 23:36:47 +0700

    fix time zone

--- src/main/java/com/coursehub/CoursehubBackendApplication.java
+++ src/main/java/com/coursehub/CoursehubBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CoursehubBackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(CoursehubBackendApplication.class, args);
 	}
 
