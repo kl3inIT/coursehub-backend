1c2db94d494adac9cfe8b72f01e410accd055027
kl3inIT <dathip04@gmail.com>
2025-07-24 21:48:26 +0700

    fix count category and add set Preview

--- src/main/java/com/coursehub/dto/request/category/CategoryRequestDTO.java
+++ src/main/java/com/coursehub/dto/request/category/CategoryRequestDTO.java
@@ -12,10 +12,10 @@ import lombok.*;
 public class CategoryRequestDTO {
 
     @NotBlank(message = "Course category name is required")
-    @Size(max = 30, message = "Name must be at most 30 characters")
+    @Size(min = 5, max = 30, message = "Name must be between 5 and 30 characters")
     private String name;
 
     @NotBlank(message = "Course category description is required")
-    @Size(max = 200, message = "Description must be at most 200 characters")
+    @Size(min = 5, max = 200, message = "Description must be between 5 and 200 characters")
     private String description;
 }
